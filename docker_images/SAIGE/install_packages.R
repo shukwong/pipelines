5e4bcae3ad718b508150f6912275d966bca96fd1
Wendy Wong <wendy.wong@gmail.com>
2020-05-01 13:39:19 -0400

    Delete install_packages.R

--- docker_images/SAIGE/install_packages.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "roxygen2", "rversions","devtools")
-for (pack in req_packages) {
-    if(!require(pack, character.only = TRUE)) {
-        install.packages(pack, repos = "https://cloud.r-project.org")
-    }
-}
